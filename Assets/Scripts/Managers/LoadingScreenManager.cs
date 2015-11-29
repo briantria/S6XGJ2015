dc30b50bb38956f4c9ea43cc772685a56900c35a
Brian Tria <btria959@gmail.com>
2015-11-30 06:52:33 +0800

    started coding for loading screen fade in / out

--- Assets/Scripts/Managers/LoadingScreenManager.cs
+++ Assets/Scripts/Managers/LoadingScreenManager.cs
@@ -14,20 +14,58 @@ public class LoadingScreenManager : MonoBehaviour
     public  static LoadingScreenManager Instance {get {return m_instance;}}
     
     private Canvas m_canvas = null;
+    private CanvasRenderer m_canvasRenderer = null;
     
     protected void Awake ()
     {
         m_instance = this;
         m_canvas = this.GetComponent<Canvas> ();
+        m_canvasRenderer = this.GetComponent<CanvasRenderer> ();
     }
     
     public void Open ()
     {
         m_canvas.enabled = true;
+        //StartCoroutine (FadeIn ());
     }
     
     public void Close ()
     {
         m_canvas.enabled = false;
+        //StartCoroutine (FadeOut ());
+    }
+    
+    private IEnumerator FadeIn ()
+    {
+        float alpha = 0.0f;
+        float speed = 20.0f;
+        m_canvasRenderer.SetAlpha (alpha);
+        
+        while (alpha < 1.0f)
+        {
+            alpha += (Time.deltaTime * speed);
+            m_canvasRenderer.SetAlpha (alpha);
+            alpha = m_canvasRenderer.GetAlpha ();
+            yield return new WaitForEndOfFrame ();
+        }
+        
+        m_canvasRenderer.SetAlpha (1.0f);
+    }
+    
+    private IEnumerator FadeOut ()
+    {
+        float alpha = 1.0f;
+        float speed = 10.0f;
+        m_canvasRenderer.SetAlpha (alpha);
+        
+        while (alpha > 0.0f)
+        {
+            alpha -= (Time.deltaTime * speed);
+            m_canvasRenderer.SetAlpha (alpha);
+            alpha = m_canvasRenderer.GetAlpha ();
+            yield return new WaitForEndOfFrame ();
+        }
+        
+        m_canvasRenderer.SetAlpha (0.0f);
     }
 }
