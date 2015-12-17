97a4daa939eb692c01c53f2a287b9666fd1685af
Brian Tria <btria959@gmail.com>
2015-12-17 11:17:38 +0800

    implement simple camera follow

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -9,5 +9,26 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
+//	private Camera m_mainCamera;
+	private Transform m_transform;
+	private Transform m_playerTransform;
 	
+	protected void Awake ()
+	{
+//		m_mainCamera = Camera.main;
+		m_transform = this.transform;
+		m_playerTransform = PlayerController.Instance.transform;
+	}
+
+	protected void Update ()
+	{
+		if (GameManager.Instance != null
+		&&  GameManager.Instance.CurrentGamePhase == GamePhase.Play)
+		{
+			Vector3 position = m_transform.position;
+			position.x = m_playerTransform.position.x;
+			position.y = m_playerTransform.position.y;
+			m_transform.position = position;
+		}
+	}
 }
